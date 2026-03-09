674b7fcfd84887ea19270517a5e805a64604bdfb
andycdev <tech.andresb@gmail.com>
2026-03-09 18:11:01 -0500

    update .js

--- api/contact.js
+++ api/contact.js
@@ -1,20 +1,20 @@
 /* global process */
-import { Resend } from 'resend';
+import { Resend } from "resend";
 
 const resend = new Resend(process.env.API_KEY);
 
 export default async function handler(req, res) {
   // Solo permitir método POST
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Método no permitido' });
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Método no permitido" });
   }
 
   const { name, email, message } = req.body;
 
   try {
     const data = await resend.emails.send({
-      from: 'Kufta Contact <onboarding@resend.dev>', // Resend te da este por defecto
-      to: ['tu-correo@gmail.com'], // A donde quieres que llegue
+      from: "Kufta Contact <onboarding@resend.dev>", // Resend te da este por defecto
+      to: ["tech.andresb@gmail.com"], // A donde quieres que llegue
       subject: `Nuevo mensaje de ${name}`,
       html: `
         <h2>Nuevo mensaje desde Kufta Software</h2>
